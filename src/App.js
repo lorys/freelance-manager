6d9c1bae45565f89e16a859fd1e2b9b524bd9614
Lorys Lopez <llopez@iMac-de-Lorys.local>
2020-10-09 21:38:53 +0200

    implementing simple navigation system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{ useState } from 'react';
 import './App.css';
+import Menu from './components/Menu';
 import Home from './components/Home';
 
 const { ipcRenderer, remote } = window.require('electron');
@@ -10,9 +11,15 @@ function App() {
   const [ screen, setScreen ] = useState("home");
   
   const screens = new Array();
-  screens['home'] = <Home navigate={setScreen} currentScreen={screen}/>
+  screens['home'] = <Home />
 
-  return (screens[screen]);
+  const Screen = () => screens[screen];
+  return (
+    <React.Fragment>
+      <Menu navigate={setScreen}/>
+      <Screen />
+    </React.Fragment>
+  );
 }
 
 export default App;
