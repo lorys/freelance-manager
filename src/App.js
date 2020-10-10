a768065f4b02f1bc34a59508ce93f6ec3c3b5df9
lorys <loryslopez@hotmail.fr>
2020-10-10 20:50:21 +0200

    adding css CDN

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Home from './components/Home';
 const { ipcRenderer, remote } = window.require('electron');
 const fs = remote.require("fs");
 
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+document.head.appendChild(styleLink);
 
 function App() {
   const [ screen, setScreen ] = useState("home");
   
   const screens = new Array();
-  screens['home'] = <Home />
+  screens['home'] = <Home navigate={setScreen}/>
 
   const Screen = () => screens[screen];
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,44 @@
 import React,{ useState } from 'react';
+import { Icon, TextField } from '@material-ui/core';
 import '../App.css';
 
-function Home() {
+const { ipcRenderer, remote } = window.require('electron');
+const fs = remote.require("fs");
+
+function DevisList() {
+    if (!fs.existsSync("./data/devis/"))
+        return (<div><h2>Vous n'avez aucun devis pour l'instant.</h2></div>);
+    return (<div></div>);
+}
+
+function ClientList() {
+    if (!fs.existsSync("./data/customers/"))
+        return (<div><h2>Vous n'avez aucun clients pour l'instant.</h2></div>);
+    return (<div></div>);
+}
+
+function AddClient() {
     return (
-        <div className="homescreen xcenter"><h1>Bienvenu(e) !</h1><p>Vous trouverez ici tout ce dont vous avez besoin pour gérer vos affaires !</p></div>
+        <form>
+            <TextField id="standard-basic" label="Entreprise" />
+            <TextField id="standard-basic" label="Siret" />
+            <TextField id="standard-basic" label="rue" />
+        </form>
+    );
+}
+
+function Home({ navigate }) {
+    const [addClient , setAddClient] = useState(0);
+    return (
+        <div className="homescreen xcenter">
+            <h1>Clients <Icon onClick={() => { setAddClient(!addClient); }}>add_circle</Icon></h1>
+            <hr/>
+            {(addClient) ? <AddClient /> : ""}
+            <ClientList />
+            <h1>Devis</h1>
+            <hr/>
+            <DevisList />
+        </div>
     );
 }
 
